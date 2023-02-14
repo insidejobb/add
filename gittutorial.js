e1bf270e22752f9d27efc7ad2286a60ea1a5d4fb
MailapilliBhavani <bhavanii1924@gmail.com>
2023-02-14 16:24:45 +0530

    secondfile

---
+++ gittutorial.js
@@ -0,0 +1,2 @@
+
+console.log('bhavani')
---
+++ square.java
@@ -0,0 +1,15 @@
+public class square{
+    
+    public static void main(String args[]){
+        int num=49;
+        int count=1;
+        while(count*count<num){
+            System.out.println(count*count);
+            count+=1;
+            
+            
+        }
+        
+        
+    }
+}
