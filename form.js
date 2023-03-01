86b4f2b4be1f8fc202df43c1bd8baf211c0b2664
MailapilliBhavani <bhavanii1924@gmail.com>
2023-03-01 18:51:47 +0530

    localstorage

---
+++ form.js
@@ -0,0 +1,6 @@
+function localstorage(event){
+    let name=event.target.fname.value
+    let lastname=event.target.Lname.value
+    event.preventDefault()
+     localStorage.setItem('Name',name)
+}
